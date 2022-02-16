449805b55440edd926369b06719568eaa6726e70
Pedro Machado <phfm@cin.ufpe.br>
2022-02-16 19:56:18 -0300

    feat: incluindo context de temas do mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 
 import AppRoutes from "./routes";
 
+const theme = createTheme();
+
 function App(): JSX.Element {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
