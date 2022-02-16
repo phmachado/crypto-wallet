31377e2f7cdeb478582b3e4b4e4bdd64560fb788
Pedro Machado <phfm@cin.ufpe.br>
2022-02-15 23:15:47 -0300

    feat: inclusão do react router no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+
+import AppRoutes from "./routes";
+
 function App(): JSX.Element {
-  return <div />;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
