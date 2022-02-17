d68f85f106692eaabc7fbe986d0529e20c3019de
Pedro Machado <phfm@cin.ufpe.br>
2022-02-16 22:40:02 -0300

    feat: inclusão da página de signup nas rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 
 import Login from "../pages/Login";
 import PageNotFound from "../pages/PageNotFound";
+import SignUp from "../pages/SignUp";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/cadastro" element={<SignUp />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
