a353a297a974ca0b1de8a0f924562a8950adf8f2
Pedro Machado <phfm@cin.ufpe.br>
2022-02-16 22:38:13 -0300

    refactor: ajustes no componente de layout para login e signup

--- src/components/CredentialsLayout.tsx
+++ src/components/CredentialsLayout.tsx
@@ -3,39 +3,33 @@ import { Box, Grid, Paper, Typography } from "@mui/material";
 import loginBg from "../assets/login-bg.jpeg";
 
 type Props = {
-  children: JSX.Element;
+  children: JSX.Element | JSX.Element[];
 };
 
-export default function BeforeAppLayout({ children }: Props): JSX.Element {
+export default function CredentialsLayout({ children }: Props): JSX.Element {
   return (
-    <Grid container component="main" sx={{ height: "100vh" }}>
+    <Grid container sx={{ height: "100vh" }}>
       <Grid
         item
-        xs={false}
         sm={4}
         md={7}
         sx={{
           backgroundImage: `url(${loginBg})`,
           backgroundRepeat: "no-repeat",
-          backgroundColor: (t) =>
-            t.palette.mode === "light"
-              ? t.palette.grey[50]
-              : t.palette.grey[900],
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={12} square>
         <Box
           sx={{
-            my: 25,
-            mx: 4,
+            p: 10,
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
           }}
         >
-          <Typography component="h1" variant="h5">
+          <Typography m={4} variant="h5">
             CryptoWallet
           </Typography>
           {children}
