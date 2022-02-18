1339322bf12862293d20bf8e1633930c65747926
Pedro Machado <phfm@cin.ufpe.br>
2022-02-18 00:56:47 -0300

    feat: inclusão das rotas para as páginas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 
+import Buy from "../pages/Buy";
 import Dashboard from "../pages/Dashboard";
+import Exchange from "../pages/Exchange";
+import History from "../pages/History";
 import Login from "../pages/Login";
 import PageNotFound from "../pages/PageNotFound";
 import SignUp from "../pages/SignUp";
@@ -11,6 +14,9 @@ function AppRoutes() {
       <Route path="/" element={<Login />} />
       <Route path="/cadastro" element={<SignUp />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/comprar" element={<Buy />} />
+      <Route path="/trocar" element={<Exchange />} />
+      <Route path="/extrato" element={<History />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
