e22dde5a60e279a72a074eba376a58f611f96931
Pedro Machado <phfm@cin.ufpe.br>
2022-02-18 00:25:08 -0300

    feat: incluindo cor personalizada para o bg do tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+import { grey } from "@mui/material/colors";
 import { BrowserRouter } from "react-router-dom";
 
 import AppRoutes from "./routes";
 
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    background: {
+      default: grey[100],
+    },
+  },
+});
 
 function App(): JSX.Element {
   return (
