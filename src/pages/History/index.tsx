4daa50a89d06c3c0b18e775bd2bd58abca6bc2c9
Pedro Machado <phfm@cin.ufpe.br>
2022-02-19 15:01:54 -0300

    feat: tabela para a página de extrato

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,61 @@
 import { Container, Grid, Paper, Typography, Box } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 import AppLayout from "../../components/AppLayout";
+
+const columns: GridColDef[] = [
+  {
+    field: "date",
+    headerName: "Data",
+    width: 150,
+  },
+  {
+    field: "operation",
+    headerName: "Operação",
+    width: 250,
+  },
+  {
+    field: "value",
+    headerName: "Valor",
+    width: 150,
+  },
+  {
+    field: "valueInReais",
+    headerName: "Valor em Reais",
+    width: 150,
+  },
+];
+
+const rows = [
+  {
+    id: 1,
+    date: "19/02/2022",
+    operation: "Venda de Bitcoin",
+    value: 0.85251,
+    valueInReais: 50000,
+  },
+  {
+    id: 2,
+    date: "19/02/2022",
+    operation: "Venda de Brita",
+    value: 50000,
+    valueInReais: 50000,
+  },
+  {
+    id: 3,
+    date: "19/02/2022",
+    operation: "Troca de Brita por Bitcoin",
+    value: 5000,
+    valueInReais: 50000,
+  },
+  {
+    id: 4,
+    date: "19/02/2022",
+    operation: "Troca de Bitcoin por Brita",
+    value: 0.001,
+    valueInReais: 50000,
+  },
+];
 
 export default function History(): JSX.Element {
   return (
@@ -17,7 +72,19 @@ export default function History(): JSX.Element {
                 display: "flex",
                 flexDirection: "column",
               }}
-            />
+            >
+              <Box sx={{ height: 400, width: "100%" }}>
+                <DataGrid
+                  rows={rows}
+                  columns={columns}
+                  pageSize={5}
+                  rowsPerPageOptions={[5]}
+                  disableSelectionOnClick
+                  disableColumnSelector
+                  disableColumnMenu
+                />
+              </Box>
+            </Paper>
           </Grid>
         </Grid>
       </Container>
