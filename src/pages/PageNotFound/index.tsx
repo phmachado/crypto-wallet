92da8303d7793a64e379898d1589c857a8c7ee93
Pedro Machado <phfm@cin.ufpe.br>
2022-02-19 17:00:27 -0300

    feat: estilização da 404 page

--- src/pages/PageNotFound/index.tsx
+++ src/pages/PageNotFound/index.tsx
@@ -1,5 +1,21 @@
-function PageNotFound(): JSX.Element {
-  return <div>404 - Page Not Found</div>;
-}
+import { Typography, Box } from "@mui/material";
 
-export default PageNotFound;
+export default function PageNotFound(): JSX.Element {
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Typography variant="h1" sx={{ paddingBottom: 3 }}>
+        Oops.
+      </Typography>
+      <Typography variant="button">Página não encontrada :/</Typography>
+    </Box>
+  );
+}
