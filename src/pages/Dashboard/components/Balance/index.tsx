a9cd95d5c7f7d3ac339ea4800bc959d8fddc33c3
Pedro Machado <phfm@cin.ufpe.br>
2022-02-19 15:56:20 -0300

    feat: criação do componente de saldo

--- src/pages/Dashboard/components/Balance/index.tsx
+++ src/pages/Dashboard/components/Balance/index.tsx
@@ -1,3 +1,26 @@
+import { AccountBalanceWalletOutlined } from "@mui/icons-material";
+import { Box, Typography } from "@mui/material";
+
 export default function Balance() {
-  return <>Saldo</>;
+  return (
+    <Box sx={{ p: 4, display: "flex", flexDirection: "column" }}>
+      <Typography variant="h4">
+        <AccountBalanceWalletOutlined /> Saldo
+      </Typography>
+      <Typography sx={{ px: 4 }} variant="caption">
+        Em {new Date().toISOString()}
+      </Typography>
+      <Box sx={{ px: 4, paddingTop: 2 }}>
+        <Typography sx={{ marginBottom: 1 }} variant="h6">
+          Real: R$ 100.000,00
+        </Typography>
+        <Typography sx={{ marginBottom: 1 }} variant="h6">
+          Bitcoin: 0.48 BTC
+        </Typography>
+        <Typography sx={{ marginBottom: 1 }} variant="h6">
+          Brita: B$ 19459,04
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
