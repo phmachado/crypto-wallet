9059e8b6062d982dc2928cbc861dbb308059f5b2
Pedro Machado <phfm@cin.ufpe.br>
2022-02-20 20:04:17 -0300

    feat: conexão do login ao db

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,27 @@
 import { Button, Box, TextField, Link } from "@mui/material";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import CredentialsLayout from "../../components/CredentialsLayout";
+import { db } from "../../db";
 
 export default function Login(): JSX.Element {
   const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  async function handleLogin() {
+    try {
+      const userExists = await db.user.where({ email, password }).toArray();
+      if (userExists.length !== 0) {
+        navigate("/dashboard");
+      } else {
+        console.log("INCORRECT_OR_DONT_EXIST");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <CredentialsLayout>
@@ -18,6 +35,7 @@ export default function Login(): JSX.Element {
         placeholder="Digite seu e-mail"
         autoComplete="email"
         autoFocus
+        onChange={(e) => setEmail(e.target.value)}
       />
       <TextField
         margin="dense"
@@ -28,12 +46,14 @@ export default function Login(): JSX.Element {
         type="password"
         placeholder="Digite sua senha"
         autoComplete="current-password"
+        onChange={(e) => setPassword(e.target.value)}
       />
       <Button
         fullWidth
         variant="contained"
         sx={{ mt: 3, mb: 2 }}
-        onClick={() => navigate("/dashboard")}
+        onClick={() => handleLogin()}
+        disabled={email === "" || password === ""}
       >
         Entrar
       </Button>
