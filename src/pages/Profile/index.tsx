69b633b82cf633dd21ec020bb702999556e2b11b
Pedro Machado <phfm@cin.ufpe.br>
2022-02-20 21:21:07 -0300

    feat: conectando a página de perfil com o db

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -8,10 +8,41 @@ import {
   Typography,
   Grid,
 } from "@mui/material";
+import { useState, useContext, useEffect } from "react";
 
 import AppLayout from "../../components/AppLayout";
+import { UserContext } from "../../contexts/UserContext";
+import { db } from "../../db";
 
 export default function Profile(): JSX.Element {
+  const { currentUser } = useContext(UserContext);
+
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  useEffect(() => {
+    if (currentUser) {
+      setName(currentUser.name);
+      setEmail(currentUser.email);
+      setPassword(currentUser.password);
+    }
+  }, [currentUser]);
+
+  async function handleUpdateUser() {
+    try {
+      if (currentUser) {
+        await db.user.update(Number(currentUser.id), {
+          name,
+          email,
+          password,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <AppLayout>
       <Container maxWidth="lg" sx={{ mb: 4 }}>
@@ -23,49 +54,63 @@ export default function Profile(): JSX.Element {
         <Grid container spacing={3}>
           <Grid item xs={12}>
             <Paper>
-              <Box sx={{ p: 5 }}>
-                <Box
-                  sx={{
-                    width: "100%",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    paddingBottom: 2,
-                  }}
-                >
-                  <AccountCircle color="primary" sx={{ fontSize: 100 }} />
+              {currentUser ? (
+                <Box sx={{ p: 5 }}>
+                  <Box
+                    sx={{
+                      width: "100%",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      paddingBottom: 2,
+                    }}
+                  >
+                    <AccountCircle color="primary" sx={{ fontSize: 100 }} />
+                  </Box>
+                  <TextField
+                    margin="dense"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    autoFocus
+                    defaultValue={currentUser.name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <TextField
+                    margin="dense"
+                    required
+                    fullWidth
+                    type="email"
+                    id="email"
+                    label="E-mail"
+                    placeholder="Digite seu e-mail"
+                    defaultValue={currentUser.email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <TextField
+                    margin="dense"
+                    required
+                    fullWidth
+                    type="password"
+                    id="password"
+                    label="Senha"
+                    placeholder="Digite sua senha"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    onClick={() => handleUpdateUser()}
+                  >
+                    Atualizar
+                  </Button>
                 </Box>
-                <TextField
-                  margin="dense"
-                  required
-                  fullWidth
-                  id="name"
-                  label="Nome"
-                  placeholder="Digite seu nome"
-                  autoFocus
-                />
-                <TextField
-                  margin="dense"
-                  required
-                  fullWidth
-                  type="email"
-                  id="email"
-                  label="E-mail"
-                  placeholder="Digite seu e-mail"
-                />
-                <TextField
-                  margin="dense"
-                  required
-                  fullWidth
-                  type="password"
-                  id="password"
-                  label="Senha"
-                  placeholder="Digite sua senha"
-                />
-                <Button fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-                  Atualizar
-                </Button>
-              </Box>
+              ) : (
+                "Carregando..."
+              )}
             </Paper>
           </Grid>
         </Grid>
